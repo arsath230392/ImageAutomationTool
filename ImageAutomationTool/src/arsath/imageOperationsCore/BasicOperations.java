f07700bde33242bb73f1060d378acd32b4619467
Arsath <Arsath@ARSATH-ELITEBOOK>
2017-12-03 22:52:48 +0530

    added sendkey functionality

--- ImageAutomationTool/src/arsath/imageOperationsCore/BasicOperations.java
+++ ImageAutomationTool/src/arsath/imageOperationsCore/BasicOperations.java
@@ -1,5 +1,6 @@
 package arsath.imageOperationsCore;
 
+import java.awt.AWTException;
 import java.awt.Dimension;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
@@ -7,16 +8,21 @@ import java.awt.event.InputEvent;
 public class BasicOperations {
 	public static void performClick(String inputFilePath, int maxTimeoutInSeconds, int offsetX, int offsetY)
 			throws Exception {
-		performAction(inputFilePath, maxTimeoutInSeconds, "click", offsetX, offsetY);
+		performAction(inputFilePath, maxTimeoutInSeconds, "click", offsetX, offsetY, "");
 	}
 
 	public static void performHover(String inputFilePath, int maxTimeoutInSeconds, int offsetX, int offsetY)
 			throws Exception {
-		performAction(inputFilePath, maxTimeoutInSeconds, "hover", offsetX, offsetY);
+		performAction(inputFilePath, maxTimeoutInSeconds, "hover", offsetX, offsetY, "");
+	}
+
+	public static void sendKeys(String inputFilePath, int maxTimeoutInSeconds, int offsetX, int offsetY, String text)
+			throws Exception {
+		performAction(inputFilePath, maxTimeoutInSeconds, "send", offsetX, offsetY, text);
 	}
 
 	private static void performAction(String inputFilePath, int maxTimeoutInSeconds, String action, int offsetX,
-			int offsetY) throws Exception {
+			int offsetY, String text) throws Exception {
 		Robot rbt = new Robot();
 		long currentTimeInMillis = System.currentTimeMillis();
 		long maxTimeout = currentTimeInMillis + (maxTimeoutInSeconds * 1000);
@@ -35,11 +41,32 @@ public class BasicOperations {
 				} else if (action.equals("hover")) {
 					rbt.mouseMove(targetX, targetY);
 					return;
+				} else if (action.equals("send")) {
+					rbt.mouseMove(targetX, targetY);
+					rbt.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+					rbt.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+					type(text);
+					return;
 				}
 
 			}
 		} while (currentTimeInMillis < maxTimeout);
 		throw new ImageNotFoundException(
 				"Image '" + inputFilePath + "'not found in Screen.Wait Time:" + maxTimeoutInSeconds);
+	}
+
+	private static void type(String text) throws AWTException {
+		Robot rbt = new Robot();
+		byte[] bytes = text.getBytes();
+		for (byte b : bytes) {
+			int code = b;
+			// keycode only handles [A-Z] (which is ASCII decimal [65-90])
+			if (code > 96 && code < 123)
+				code = code - 32;
+			rbt.delay(40);
+			rbt.keyPress(code);
+			rbt.keyRelease(code);
+		}
+
 	}
 }
--- ImageAutomationTool/src/arsath/test/Test.java
+++ ImageAutomationTool/src/arsath/test/Test.java
@@ -5,138 +5,21 @@ import arsath.imageOperationsCore.BasicOperations;
 public class Test {
 	public static void main(String[] args) {
 		try {
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture.PNG", 10, 0,
-					0);
-			BasicOperations.performHover(
-					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\Capture2.PNG", 10, 0,
-					0);
+			BasicOperations.sendKeys(
+					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\1.PNG", 10, 100, 0,
+					"Hi");
+			BasicOperations.sendKeys(
+					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\1.PNG", 10, 100, 0,
+					"Hi");
+			BasicOperations.sendKeys(
+					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\1.PNG", 10, 100, 0,
+					"Hi");
+			BasicOperations.sendKeys(
+					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\1.PNG", 10, 100, 0,
+					"Hi");
+			BasicOperations.sendKeys(
+					"E:\\workspace\\GIT\\ImageAutomationTool\\ImageAutomationTool\\Sample image\\1.PNG", 10, 100, 0,
+					"Hi");
 		} catch (Exception e) {
 
 			e.printStackTrace();
