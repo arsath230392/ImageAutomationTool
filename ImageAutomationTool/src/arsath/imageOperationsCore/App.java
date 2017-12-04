0f5503edca03712c8caaef7b09ba31b8748bef29
Arsath <Arsath@ARSATH-ELITEBOOK>
2017-12-05 02:25:47 +0530

    added comments to the app class

--- ImageAutomationTool/src/arsath/imageOperationsCore/App.java
+++ ImageAutomationTool/src/arsath/imageOperationsCore/App.java
@@ -6,10 +6,12 @@ public class App {
 	private Runtime runtime = Runtime.getRuntime();
 	private Process prcs;
 
+	/** opens a executable program */
 	public void openApp(String appPath) throws IOException {
 		prcs = runtime.exec(appPath);
 	}
 
+	/** closes the opened process */
 	public void closeApp() {
 		prcs.destroyForcibly();
 	}
